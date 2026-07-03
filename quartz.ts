1ccadd2dc52f893d64e35941fd70e5c4117d4c3e
Philipp <philipp.henke98@outlook.de>
2026-07-03 11:14:03 +0200

    Quartz sync: Jul 3, 2026, 11:14 AM

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,16 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 
-const config = await loadQuartzConfig()
+const config = await loadQuartzConfig({
+  head: [
+    {
+      tag: "meta",
+      attrs: {
+        "http-equiv": "Content-Security-Policy",
+        content: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; frame-src https://www.youtube.com https://www.youtube-nocookie.com; img-src 'self' data: https:; connect-src 'self' https:;",
+      },
+    },
+  ],
+})
+
 export default config
 export const layout = await loadQuartzLayout()
